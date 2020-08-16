87646f8791ff3c4331b1f1009bf92789672ed228
MeehirNaik <69512612+MeehirNaik@users.noreply.github.com>
2020-08-16 22:21:41 +0530

    1. token and operator

---
+++ challenge_token_and_operator/product of two numbers.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int a,b;
+
+int main()
+{
+	std::cout<<"Enter the two numbers of which product is wanted"<<std::endl;
+	std::cin>>a>>b;
+	std::cout<<std::endl;
+	std::cout<<std::endl;
+	std::cout<<"The products of "<<a<<" and "<<b<<" is : "<<(a*b)<<std::endl;
+}
---
+++ challenge_token_and_operator/size of.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int a;
+char b;
+float c;
+double d;
+
+int main()
+{
+	std::cout<<"Size of int, char, float and double are"<<std::endl;
+	std::cout<<std::endl;
+	std::cout<<std::endl;
+	std::cout<<"int    : "<<sizeof(a)<<std::endl<<"char   : "<<sizeof(b)<<std::endl<<"float  : "<<sizeof(c)<<std::endl<<"double : "<<sizeof(d)<<std::endl;
+}
